b1490bd018dd22f9bd273c3b340b3c76679c226c
Aviral Saxena <avisaxena33@gmail.com>
2018-09-12 20:36:14 -0500

    Solved.

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,12 +35,21 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (!maze.isFinished()) {
+                maze.turnRight();
+                while (!(maze.canMove())) {
+                    maze.turnLeft();
+                }
+                maze.move();
+            }
         }
-
-        if (maze.isFinished()) {
-            System.out.println("You solved the maze!");
-        } else {
-            System.out.println("Try again!");
+            if (maze.isFinished()) {
+                System.out.println("You solved the maze!");
+            } else {
+                System.out.println("Try again!");
+            }
         }
     }
-}
+
+
+
